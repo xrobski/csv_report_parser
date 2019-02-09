816d443d5c005160d239d16815bc0356af0bbfc2
xrobski <chrzmichal@gmail.com>
2019-02-09 13:37:13 +0100

    Data parsing finished

--- csv_report_parser.py
+++ csv_report_parser.py
@@ -5,10 +5,6 @@ from pycountry import subdivisions
 
 
 def csv_report_parser(file):
-    # output variables
-    csv_output = []
-    work_output = {}
-
     # file open, read csv file
     with open(file, "r", encoding="utf-8-sig") as csv_file:
         csv_reader = csv.reader(csv_file)
@@ -16,16 +12,45 @@ def csv_report_parser(file):
         # listed data
         #TODO: czy nie ma pustych wierszy?
         rows = list(csv_reader)
-
-        # sort file
         sorted_rows = sorted(rows, key=lambda x: datetime.strptime(x[0], '%m/%d/%Y'))
 
-        current_day = sorted_rows[0][0]
-        for row in sorted_rows:
-            date, city, ads, ctr = row[0], row[1], row[2], row[3]
-            #if current_day = date:
-            print(date, city, ads, ctr)
+        output_data = []
+        aggregated_data = {}
+        previous_date = sorted_rows[0][0]
 
+        for row in sorted_rows:
+            current_date, city, ads, ctr = row[0], row[1], int(row[2]), row[3]
+            new_day = previous_date != current_date
+
+            if new_day:
+                # Finalize previous day
+                for code in sorted(aggregated_data):
+                    d = datetime.strptime(previous_date, '%m/%d/%Y')
+                    formatted_date = d.strftime('%Y-%m-%d')
+                    aggreated_ads = aggregated_data[code][0]
+                    rounded_clicks = round(aggregated_data[code][1])
+
+                    # Push to output
+                    output_data.append('{},{},{},{}'.format(formatted_date,
+                                                            code,
+                                                            aggreated_ads,
+                                                            rounded_clicks))
+                # Clean up
+                aggregated_data = {}
+                previous_date = current_date
+
+            # process data
+            country_code = subdivisions.lookup(city).country_code
+            clicks = float(ctr[:-1]) / 100 * ads
+
+            # aggregate data
+            if country_code in aggregated_data:
+                aggregated_data[country_code][0] += ads
+                aggregated_data[country_code][1] += clicks
+            else:
+                aggregated_data[country_code] = [ads,clicks]
+
+        pprint(output_data)
 
 if __name__ == '__main__':
     csv_report_parser('test1.csv')
